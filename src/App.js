6046593b1ee44d7d1b7a67bf26645a00ccf9a07e
Hamza <hamza.eljoauhari.1995@gmail.com>
2023-12-16 20:57:52 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { HubConnectionBuilder } from '@microsoft/signalr';
 
-function App() {
+const ProgressBar = ({ progress }) => (
+  <div style={{ width: '100%', backgroundColor: '#ddd' }}>
+    <div style={{ width: `${progress}%`, backgroundColor: 'green', height: '20px' }} />
+  </div>
+);
+
+const App = () => {
+  const [progress, setProgress] = useState(0);
+  const [connection, setConnection] = useState(null);
+
+  useEffect(() => {
+    const newConnection = new HubConnectionBuilder()
+      .withUrl('https://localhost:7104/progressHub') // Replace with your server URL
+      .withAutomaticReconnect()
+      .build();
+
+    setConnection(newConnection);
+  }, []);
+
+  useEffect(() => {
+    if (connection) {
+      connection.start()
+        .then(() => {
+          console.log('Connected to SignalR Hub!');
+          connection.invoke('StartTask'); // Start the simulated task
+
+          connection.on('ReceiveProgress', (progress) => {
+            setProgress(progress); // Update progress state
+          });
+        })
+        .catch(error => console.error('SignalR Connection Error: ', error));
+    }
+
+    return () => {
+      if (connection) {
+        connection.stop();
+      }
+    };
+  }, [connection]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task Progress</h1>
+      <ProgressBar progress={progress} />
     </div>
   );
-}
+};
 
 export default App;
